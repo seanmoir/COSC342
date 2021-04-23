8f7129f2bdecca5e46648c6d798bbf0c27a32776
Sean Moir <moise605@student.otago.ac.nz>
2021-04-24 00:38:45 +1200

    cylinder centre finished

--- assignment2/Cylinder.cpp
+++ assignment2/Cylinder.cpp
@@ -2,26 +2,29 @@
 
 #include "utility.h"
 
-Cylinder::Cylinder() : Object() {
-
+Cylinder::Cylinder() : Object()
+{
 }
 
-Cylinder::Cylinder(const Cylinder& cylinder) : Object(cylinder) {
-
+Cylinder::Cylinder(const Cylinder &cylinder) : Object(cylinder)
+{
 }
 
-Cylinder::~Cylinder() {
-
+Cylinder::~Cylinder()
+{
 }
 
-Cylinder& Cylinder::operator=(const Cylinder& cylinder) {
-	if (this != &cylinder) {
+Cylinder &Cylinder::operator=(const Cylinder &cylinder)
+{
+	if (this != &cylinder)
+	{
 		Object::operator=(cylinder);
 	}
 	return *this;
 }
 
-std::vector<RayIntersection> Cylinder::intersect(const Ray& ray) const {
+std::vector<RayIntersection> Cylinder::intersect(const Ray &ray) const
+{
 
 	std::vector<RayIntersection> result;
 
@@ -29,59 +32,78 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray& ray) const {
 
 	// Intersection is of the form ad^2 + bd + c, where d = distance along the ray
 
-	const Point& p = inverseRay.point;
-	const Direction& d = inverseRay.direction;
-	double a = (d * p[0] * p[0]) + (d * p[1])
-	double b = 2 * p
-	double c = p.dot(p) - 1;
+	const Point &p = inverseRay.point;
+	const Direction &d = inverseRay.direction;
+	double a = (d(0) * d(0)) + (d(1) * d(1));
+	double b = 2 * ((p(0) * d(0)) + (p(1) * d(1)));
+	double c = (p(0) * p(0)) + (p(1) * p(1) - 1);
 
 	RayIntersection hit;
 	hit.material = material;
 
 	double b2_4ac = b * b - 4 * a * c;
 	double t;
-	switch (sign(b2_4ac)) {
+	switch (sign(b2_4ac))
+	{
 	case -1:
 		// No intersections
 		break;
 	case 0:
 		// One intersection
 		t = -b / (2 * a);
-		if (t > 0) {
-			// Intersection is in front of the ray's start point
-			hit.point = transform.apply(Point(p + t * d));
-			hit.normal = transform.apply(Normal(p + t * d));
-			if (hit.normal.dot(ray.direction) > 0) {
-				hit.normal = -hit.normal;
+		if (t > 0)
+		{
+			hit.point = p + t * d;
+			if (hit.point(2) <= 1 && hit.point(2) >= -1)
+			{
+				// Intersection is in front of the ray's start point
+				hit.point = transform.apply(hit.point);
+				hit.normal = transform.apply(Normal(1, 1, 0));
+				if (hit.normal.dot(ray.direction) > 0)
+				{
+					hit.normal = -hit.normal;
+				}
+				hit.distance = (hit.point - ray.point).norm();
+				result.push_back(hit);
 			}
-			hit.distance = (hit.point - ray.point).norm();
-			result.push_back(hit);
 		}
 		break;
 	case 1:
 		// Two intersections
 		t = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-		if (t > 0) {
-			// Intersection is in front of the ray's start point
-			hit.point = transform.apply(Point(p + t * d));
-			hit.normal = transform.apply(Normal(p + t * d));
-			if (hit.normal.dot(ray.direction) > 0) {
-				hit.normal = -hit.normal;
+		if (t > 0)
+		{
+			hit.point = p + t * d;
+			if (hit.point(2) <= 1 && hit.point(2) >= -1)
+			{
+				// Intersection is in front of the ray's start point
+				hit.point = transform.apply(hit.point);
+				hit.normal = transform.apply(Normal(1, 1, 0));
+				if (hit.normal.dot(ray.direction) > 0)
+				{
+					hit.normal = -hit.normal;
+				}
+				hit.distance = (hit.point - ray.point).norm();
+				result.push_back(hit);
 			}
-			hit.distance = (hit.point - ray.point).norm();
-			result.push_back(hit);
 		}
 
 		t = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-		if (t > 0) {
-			// Intersection is in front of the ray's start point
-			hit.point = transform.apply(Point(p + t * d));
-			hit.normal = transform.apply(Normal(p + t * d));
-			if (hit.normal.dot(ray.direction) > 0) {
-				hit.normal = -hit.normal;
+		if (t > 0)
+		{
+			hit.point = p + t * d;
+			if (hit.point(2) <= 1 && hit.point(2) >= -1)
+			{
+				// Intersection is in front of the ray's start point
+				hit.point = transform.apply(hit.point);
+				hit.normal = transform.apply(Normal(1, 1, 0));
+				if (hit.normal.dot(ray.direction) > 0)
+				{
+					hit.normal = -hit.normal;
+				}
+				hit.distance = (hit.point - ray.point).norm();
+				result.push_back(hit);
 			}
-			hit.distance = (hit.point - ray.point).norm();
-			result.push_back(hit);
 		}
 		break;
 	default:
@@ -90,6 +112,12 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray& ray) const {
 		exit(-1);
 		break;
 	}
-	
+
+
+	//END CAPS
+	a = (d(0) * d(0)) + (d(1) * d(1));
+	b = 2 * ((p(0) * d(0)) + (p(1) * d(1)));
+	c = (p(0) * p(0)) + (p(1) * p(1) - 1);
+
 	return result;
 }
