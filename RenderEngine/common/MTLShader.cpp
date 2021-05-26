ca35b180141039ec77f316f5153aa53497954c71
Sean Moir <moise605@student.otago.ac.nz>
2021-05-26 14:08:13 +1200

    WIP params to shader

--- RenderEngine/common/MTLShader.cpp
+++ RenderEngine/common/MTLShader.cpp
@@ -32,43 +32,46 @@ void MTLShader::setUpShaderParameters(){
     GLint diffusecolorID = glGetUniformLocation(programID, "diffuseColor");
     glProgramUniform4fv(programID,diffusecolorID,1, &m_diffuseColor[0]);
 
+    m_TextureID  = glGetUniformLocation(programID, "myTextureSampler");
+
     m_texture =NULL;
 }
 
 void MTLShader::setTexture(Texture* texture){
     m_texture = texture;
-    // Get a handle for our "myTextureSampler" uniform
-	
 }
 
 
 void MTLShader::setLightPos(glm::vec3 lightPos){
-    
     m_lightPos= lightPos;
-	
+    m_lightPosID = glGetUniformLocation(programID, "lightPosWorldspace");
+    glUniform3f(m_lightPosID, m_lightPos.x, m_lightPos.y, m_lightPos.z);
 }
 
 void MTLShader::setDiffuse(glm::vec3 diffuse){
     
-    m_diffuseColor= glm::vec4(diffuse[0],diffuse[1],diffuse[2],1.0);;
-    GLint diffusecolorID = glGetUniformLocation(programID, "diffuseColor");
+    m_diffuseColor= glm::vec4(diffuse[0],diffuse[1],diffuse[2],1.0);
+    GLint diffusecolorID = glGetUniformLocation(programID, "diffuseLightColor");
     glProgramUniform4fv(programID,diffusecolorID,1, &m_diffuseColor[0]);
 }
 void MTLShader::setAmbient(glm::vec3 ambient){
     
     m_ambientColor= glm::vec4(ambient[0],ambient[1],ambient[2],1.0);
+    GLint ambientcolorID = glGetUniformLocation(programID, "ambientLightColor");
+    glProgramUniform4fv(programID,ambientcolorID,1, &m_ambientColor[0]);
 	
 }
 void MTLShader::setSpecular(glm::vec3 specular){
     
     m_specularColor= glm::vec4(specular[0],specular[1],specular[2],1.0);
+    GLint specularcolorID = glGetUniformLocation(programID, "specularLightColor");
+    glProgramUniform4fv(programID,specularcolorID,1, &m_specularColor[0]);
 	
 }
 
 void MTLShader::setOpacity(float opacity){
     
     m_opacity= opacity;
-	
 }
 
 void MTLShader::setRenderMode(float renderMode){
